2f65780c3ffb4b66ab4adb82ab906bc4177e9539
Stanislav Erokhin <erokhins239@gmail.com>
2014-07-30 00:13:58 +0400

    Add Intervals.

---
+++ Common/src/ogr/hanuna/images/common/Intervals.kt
@@ -0,0 +1,18 @@
+package ogr.hanuna.images.common
+
+/**
+ * Created by smevok on 7/27/14.
+ */
+
+public trait MatrixArea {
+    val cornerLT: Coordinates
+    val cornerRB: Coordinates
+}
+
+public fun Coordinates.rangeTo(other: Coordinates): MatrixArea {
+    assert(this.col <= other.col && this.row <= other.row)
+    return object : MatrixArea {
+        override val cornerLT: Coordinates = this@rangeTo
+        override val cornerRB: Coordinates = other
+    }
+}
